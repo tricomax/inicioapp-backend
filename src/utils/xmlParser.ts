877c86fa838ca9ce2aa71b8efd3bb1d333efebed
tricomax <tricomax@hotmail.com>
2025-01-20 16:07:00 +0100

    Se añade script build y se añade id a bookmarks.json

--- src/utils/xmlParser.ts
+++ src/utils/xmlParser.ts
@@ -5,7 +5,7 @@ export async function parseXMLToJSON(xml: string) {
     console.log("Parsing XML to JSON...");
     const result = await parseStringPromise(xml);
     console.log("XML parsed to JSON successfully.");
-    return transformBookmarks(result.xbel); // Accede a la raíz xbel
+    return transformBookmarks(result.xbel);
   } catch (error) {
     console.error("Error parsing XML to JSON:", error);
     throw error;
@@ -13,18 +13,18 @@ export async function parseXMLToJSON(xml: string) {
 }
 
 function transformBookmarks(data: any): any {
-  // console.log("Transforming:", JSON.stringify(data, null, 2));
-
   if (data.folder) {
     return data.folder.map((folder: any) => ({
+      id: folder.$.id,
       title: folder.title[0],
       type: "folder",
       children: folder.bookmark
         ? folder.bookmark
             .map((bookmark: any) => ({
+              id: bookmark.$.id,
               title: bookmark.title[0],
               url: bookmark.$.href,
-              type: "bookmark",
+      type: "bookmark",
             }))
             .concat(
               folder.folder ? transformBookmarks(folder) : []
@@ -35,6 +35,7 @@ function transformBookmarks(data: any): any {
     }));
   } else if (data.bookmark) {
     return data.bookmark.map((bookmark: any) => ({
+      id: bookmark.$.id,
       title: bookmark.title[0],
       url: bookmark.$.href,
       type: "bookmark",
